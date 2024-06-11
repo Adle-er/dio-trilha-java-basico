b082d6ea82abb685c06677282fe994ba1dcde068
Adler Tawan <szadleersz@gmail.com>
2024-06-10 21:12:36 -0300

    Exercício 01

---
+++ tipo-variaveis/src/exercicio01.java
@@ -0,0 +1,49 @@
+public class exercicio01 {
+    public static void main(String[] args) {
+        // String marca = "Peugeot";
+        // String modelo = "207 XS 16v Automático";
+        // int ano = 2012;
+        // double tabelaFip = 25880;
+
+        // System.out.println ("A marca do seu carro é: " + marca);
+        // System.out.println ("O modelo do seu carro é: " + modelo);
+        // System.out.println ("O ano do seu carro é: " + ano);
+        // System.out.println ("O valor do seu carro pela tabela Fip é: " + tabelaFip);
+
+        // ==================================================================
+
+        // byte idade = 25;
+        // float altura = 1.77f;
+        // boolean casado = false;
+        // char genero = 'M';
+
+        // System.out.println("Minha idade é: " + idade);
+        // System.out.println("Minha altura é: " + altura);
+        // System.out.println("Meu estado cívil é casado: " + casado);
+        // System.out.println("Meu gênero é: " + genero);
+
+        // ==================================================================
+
+        // int a = 10;
+        // int b = 5;
+
+        // System.out.println("Adição: " + (a+b));
+        // System.out.println("Subtração: " + (a-b));
+        // System.out.println("Multiplicação: " + (a*b));
+        // System.out.println("Divisão: " + (a/b));
+        // System.out.println("Resto da divisão: " + (a%b));
+
+        // ==================================================================
+
+        // int numeroAleatorio = 165;
+
+        // String resultado = (numeroAleatorio % 2 == 0) ? "Par" : "Ímpar";
+        // System.out.println("Este número é: " + resultado);
+
+        // ==================================================================
+
+        
+
+    }
+
+}
